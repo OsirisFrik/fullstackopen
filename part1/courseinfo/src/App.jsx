f262ede8796961c2fcf7537c1dda46d9322962ef
osirisfrik <osiris.lpz96@gmail.com>
2024-04-08 21:17:54 -0700

    1.4, 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,21 +3,24 @@ import { Header } from './components/Header'
 import { Total } from './components/Total'
 
 function App() {
-  const course = 'Half Stack application development'
-  const parts = [
-    ['Fundamentals of React', 10],
-    ['Using props to pass data', 7],
-    ['State of component', 14]
-  ]
-
-  // eslint-disable-next-line no-unused-vars
-  const sumExercises = parts.reduce((acc, [_, exercises]) => acc + exercises, 0)
+  const course = {
+    title: 'Half Stack application development',
+    parts: [
+      ['Fundamentals of React', 10],
+      ['Using props to pass data', 7],
+      ['State of component', 14]
+    ],
+    total: function total() {
+      // eslint-disable-next-line no-unused-vars
+      return this.parts.reduce((acc, [_, exercises]) => acc + exercises, 0)
+    }
+  }
 
   return (
     <div>
-      <Header content={course} />
-      <Content parts={parts} />
-      <Total total={sumExercises} />
+      <Header content={course.title} />
+      <Content parts={course.parts} />
+      <Total total={course.total()} />
     </div>
   )
 }
